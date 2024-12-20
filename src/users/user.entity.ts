f2a62ef40731555909581d8e0c94a588867c3f78
Steven Orlando Chavez Rodriguez <admin_kubernetes@mintic.gov.co>
2024-12-20 11:57:44 -0500

    Backend-Conexion a la base de datos y nueva tabla

---
+++ src/users/user.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({name:'users'})
+export class User{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    lastname:string;
+
+    @Column({unique:true})
+    email:string;
+
+    @Column({unique:true})
+    phone:string;
+
+    @Column({nullable:true})
+    image:string;
+    
+    @Column()
+    password:string;
+
+    @Column({nullable:true})
+    notification_token:string;
+    
+    @Column({type: 'datetime', default: () => 'CURRENT_TIMESTAMP'})
+    created_at:string;
+
+}
