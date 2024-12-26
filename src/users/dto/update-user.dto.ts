27801f6838d652a335a475af401232a72f542b44
Steven Orlando Chavez Rodriguez <admin_kubernetes@mintic.gov.co>
2024-12-26 17:03:10 -0500

    Backend - Modificación en el update-user.dto.ts, se agrega @IsOptional()

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,19 +1,25 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto{
     
+    @IsOptional()
     @IsNotEmpty()
     @IsString()    
     name?:string;
 
-   
+    @IsOptional()
+    @IsNotEmpty()
     @IsString()    
     lastname?:string;
 
+    @IsOptional()
     @IsNotEmpty()
     @IsString()    
     phone?:string;
 
+    @IsOptional()
     image?:string;
+
+    @IsOptional()
     notification_token?:string;
 }
